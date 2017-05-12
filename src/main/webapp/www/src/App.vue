e7270fbd102d92ef043a05c5740488bcfdeebeec
mawujun1234 <mawujun1234@163.com>
2017-05-12 16:16:49 +0800

    初始化

---
+++ src/main/webapp/www/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- 存放背景图片啥的-->
+  <div id="app">
+sdfsdf
+  <img src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
