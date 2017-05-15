8f92936ad76d5efed2e4185bb4c19476b10e8696
mawujun1234 <mawujun1234@163.com>
2017-05-15 22:15:39 +0800

    jjj

--- src/main/webapp/www/src/App.vue
+++ src/main/webapp/www/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- 存放背景图片啥的-->
   <div id="app">
-sdfsdf
-
+    <div id="echart" style="height:300px;width:300px;">
+    </div>
   </div>
 </template>
 
@@ -13,14 +13,36 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted:function(){
+    // 基于准备好的dom，初始化echarts实例
+var myChart = echarts.init(document.getElementById('echart'));
+// 绘制图表
+myChart.setOption({
+    title: { text: 'ECharts 入门示例' },
+    tooltip: {},
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+});
   }
 }
 </script>
 
 <style>
-#app {
+.app_layout {
   width: 100%;
   height:100%;
   background-color: rgba(0, 0, 17,0.9);
+  position:absolute;
+  top: 0;
+  left: 0;
+  visibility: hidden;
 }
 </style>
--- src/main/webapp/www/src/main.js
+++ src/main/webapp/www/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 require('./assets/main.css');
+window.echarts = require('echarts');
 
 new Vue({
   el: '#app',
