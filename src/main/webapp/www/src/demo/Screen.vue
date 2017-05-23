cb8726eed17b4c9a17f41d3fdf78b947b6e94ee8
mawujun1234 <mawujun1234@163.com>
2017-05-23 18:04:01 +0800

    11

--- src/main/webapp/www/src/demo/Screen.vue
+++ src/main/webapp/www/src/demo/Screen.vue
@@ -15,27 +15,29 @@
     <Doughnut></Doughnut>
     <Doughnut0></Doughnut0>
     <China></China>
+    <Videoo></Videoo>
 
     <Alert></Alert>
   </div>
 </template>
 
 <script>
-import Demo from './main/Demo.vue';
-import Headerr from './main/Headerr.vue';
-import Timesale from './main/Timesale.vue';
-import Recovery from './main/Recovery.vue';
-import Rate from './main/Rate.vue';
-import RateBarComp from './main/RateBarComp.vue';
-import RateProfit from './main/RateProfit.vue';
-import TimesaleComp from './main/TimesaleComp.vue';
-import TimesaleType from './main/TimesaleType.vue';
-import Alert from './main/Alert.vue';
-import List from './main/List.vue';
-import Top from './main/Top.vue';
-import Doughnut from './main/Doughnut.vue';
-import Doughnut0 from './main/Doughnut0.vue';
-import China from './main/China.vue';
+import Demo from './componets/Demo.vue';
+import Headerr from './componets/Headerr.vue';
+import Timesale from './componets/Timesale.vue';
+import Recovery from './componets/Recovery.vue';
+import Rate from './componets/Rate.vue';
+import RateBarComp from './componets/RateBarComp.vue';
+import RateProfit from './componets/RateProfit.vue';
+import TimesaleComp from './componets/TimesaleComp.vue';
+import TimesaleType from './componets/TimesaleType.vue';
+import Alert from './componets/Alert.vue';
+import List from './componets/List.vue';
+import Top from './componets/Top.vue';
+import Doughnut from './componets/Doughnut.vue';
+import Doughnut0 from './componets/Doughnut0.vue';
+import China from './componets/China.vue';
+import Videoo from './componets/Videoo.vue';
 
 
 export default {
@@ -46,13 +48,21 @@ export default {
     }
   },
   components: { Demo,Headerr,Timesale,Recovery,Rate,RateBarComp,RateProfit,TimesaleComp,
-    TimesaleType,Alert,List,Top,Doughnut,Doughnut0,China},
+    TimesaleType,Alert,List,Top,Doughnut,Doughnut0,China,Videoo},
   mounted:function(){
 
+    //alert($(document).width());
+    //  alert($('body').width());
   }
 }
 </script>
 
 <style>
+#screen{
+  overflow-x: hidden;
+  position:absolute;
+  top:0px;
+  left:0px;
+}
 
 </style>
---
+++ src/main/webapp/www/src/demo/componets/Videoo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="comp-videoo">
+    <video src="../static/movie.ogg" controls="controls" autoplay loop>
+    你的浏览器不支持视频播放
+    </video>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  mounted: function mounted() {
+    let vm=this;
+  },
+  methods: {
+
+  }
+}
+</script>
+<style>
+.comp-videoo {
+  position:absolute;
+  width:240px;
+  height:240px;
+  top:360px;
+  left:860px;
+}
+.comp-videoo video {
+  width:100%;
+  height:100%;
+}
+</style>
